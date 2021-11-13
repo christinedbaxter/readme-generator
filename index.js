82ca31d39b3543f7ac35f636afb779a7f48f0068
Christine Baxter <christine.d.baxter@live.com>
2021-11-12 20:17:32 -0600

    capture user input

--- index.js
+++ index.js
@@ -1,13 +1,136 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const { writeToFile } = require("./utils/generateMarkdown.js");
+const generateMarkdown = require("./src/file-template.js");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    console.log(`
+    =====================
+       User Information
+    =====================
+    `);
+    return inquirer.prompt([
+    {
+        type: "input",
+        name: "githubUserName",
+        message: "What is your GitHub username? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email address? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your email address!");
+                return false;
+            }
+        }
+    }
+    ])
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptProject = readMeFileData => {
+    console.log(`
+    =======================
+      Project Information
+    =======================
+    `);
 
-// TODO: Create a function to initialize app
-function init() {}
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "projectName",
+                message: "What is your project's name? (Required)",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter a project name!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "description",
+                message: "Provide a short description of your project (Required)",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter a short description!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "list",
+                name: "license",
+                message: "What kind of license should your project have?",
+                choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+            },
+            {
+                type: "input",
+                name: "installDependency",
+                message: "What command should be run to install dependencies",
+                default: "npm i"
+            },
+            {
+                type: "input",
+                name: "runTest",
+                message: "What command should be run to run tests?",
+                default: "npm test"
+            },
+            {
+                type: "input",
+                name: "repoInfoNeeded",
+                message: "What does the user need to know about using the repo?"
+            },
+            {
+                type: "input",
+                name: "projectContributions",
+                message: "What does the user need to know about contributing to the repo?"
+            },
+            // {
+            //     type: "confirm",
+            //     name: "confirmAddProject",
+            //     message: "Would you like to enter another project?",
+            //     default: false
+            // }
+        ])
+        .then(projectData  = () =>  {            
+            return readMeFileData;
+        });
+};
 
-// Function call to initialize app
-init();
+promptUser()
+    .then(promptProject)
+    .then(readMeFileData => {
+        return generateMarkdown(readMeFileData);
+    })
+    .then(pageMarkdown => {
+        return writeToFile(pageMarkdown);
+    })
+    .then(writeToFileResponse => {
+        console.log(writeToFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,41 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require("fs");
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dist/index.html", fileContent, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's '.catch()' method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+            // if everything went well, resolve the Promise and send the successful data to the '.then()' method
+            resolve({
+                ok: true,
+                message: "File created!"
+            });
+        });
+    });
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile("./src/style.css", "./dist/style.css", err => {
+            // if there's an error, reject the Promise and send the error to the Promise's '.catch()' method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
 
-`;
-}
+            // if everything went well, resolve the Promise and send the successful data to the '.then()' method
+            resolve({
+                ok: true,
+                message: "Stylesheet created!"
+            });
+        });
+    });
+};
 
-module.exports = generateMarkdown;
+module.exports = { writeFile };
